053dad81f77e2271592c8b8c9764a7f3d202af7f
Gabriel Silva <gabriel.etefmc@gmail.com>
2021-01-18 18:22:55 -0300

    0.1

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ export default function Main() {
 
   return (
     <>
-      <h1 className="main-title">Datas</h1>
+      <h1 className="main-title">Date Calculator</h1>
       <Form
         date={date}
         setDate={setDate}
